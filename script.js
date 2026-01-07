d2483f9cba2fadf3ff13228b370ae3db7fe47086
Ashish Mishra <ashishbnm4@gmail.com>
2026-01-07 21:21:21 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const form = document.querySelector("form");
-const loader = document.querySelector(".loader");
-const textarea = document.querySelector("textarea");
-
-form.addEventListener("submit", () => {
-    loader.style.display = "block";
-});
-
-textarea.focus();
